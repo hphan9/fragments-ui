fef369e831ab0e6ce5768de843953bffeb42e5dd
“hphan9” <emilyhuyenphan@gmail.com>
2022-01-25 17:23:07 -0500

    Adding api.js to use fragments microservice API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
-
+import {getUserFragments} from './api';
 async function init() {
   // Get our UI elements
   const userSection = document.querySelector('#user');
@@ -27,6 +27,8 @@ async function init() {
     logoutBtn.disabled = true;
     return;
   }
+  // Do an authenticated request to the fragments API server and log the result
+  getUserFragments(user);
 
   // Log the user info for debugging purposes
   console.log({ user });
